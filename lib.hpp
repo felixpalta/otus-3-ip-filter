620ab759842c450aa26d8c958b9a88a20d471a20
Felix Palta (office) <fpalta90@gmail.com>
2019-07-12 17:30:17 +0300

    Fix comment

--- lib.hpp
+++ lib.hpp
@@ -15,11 +15,11 @@ std::vector<std::string> Split(const std::string & str, char d);
 
 using IpAddr = std::tuple<int, int, int, int>;
 
-// ("113.162.145.156") -> {"113", "162", "145", "56"}
+// ("113.162.145.156") -> {113, 162, 145, 56}
 // Throws std::exception if failed to parse input string
 IpAddr IpAddrFromString(const std::string & str);
 
-// {"113", "162", "145", "56"} -> ("113.162.145.156")
+// {113, 162, 145, 56} -> ("113.162.145.156")
 std::string IpAddrToString(const IpAddr & ip);
 
 enum class SortType
