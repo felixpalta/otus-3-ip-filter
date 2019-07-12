6c98b2df4b98ba2c26c6eba9f5e85e933ba35709
Felix Palta <fpalta90@gmail.com>
2019-07-12 12:23:13 +0300

    Input ip.tsv

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include "lib.hpp"
 #include <iostream>
 
-int main() {
+int main()
+{
+    try
+    {
+        std::vector<std::vector<std::string> > ip_pool;
 
-    std::cout << "Hello World!\n"
-              << "Version: " << otus::GetVersion() << std::endl;
+        for(std::string line; std::getline(std::cin, line);)
+        {
+            std::vector<std::string> v = otus::Split(line, '\t');
+            ip_pool.push_back(otus::Split(v.at(0), '.'));
+        }
+
+        // TODO reverse lexicographically sort
+
+        for(std::vector<std::vector<std::string> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
+        {
+            for(std::vector<std::string>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
+            {
+                if (ip_part != ip->cbegin())
+                {
+                    std::cout << ".";
+
+                }
+                std::cout << *ip_part;
+            }
+            std::cout << std::endl;
+        }
+
+    }
+    catch(const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
     return 0;
 }
