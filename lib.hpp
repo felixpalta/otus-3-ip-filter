9a32b427089449adf9d454d69196fc0f2934ca35
Felix Palta <fpalta90@gmail.com>
2019-07-12 12:15:40 +0300

    Add split function

--- lib.hpp
+++ lib.hpp
@@ -1,6 +1,16 @@
 #pragma once
+#include <string>
+#include <vector>
 
 namespace otus {
+
+// ("",  '.') -> [""]
+// ("11", '.') -> ["11"]
+// ("..", '.') -> ["", "", ""]
+// ("11.", '.') -> ["11", ""]
+// (".11", '.') -> ["", "11"]
+// ("11.22", '.') -> ["11", "22"]
+std::vector<std::string> Split(const std::string &str, char d);
 
 int GetVersion();
 
--- tests_default.cpp
+++ tests_default.cpp
@@ -4,3 +4,24 @@
 TEST(DefaultTests,TestVersion) {
     ASSERT_GT(otus::GetVersion(), 0);
 }
+
+TEST(DefaultTests, TestSplit) {
+    struct TestData
+    {
+        std::string instr;
+        char inchar;
+        std::vector<std::string> out;
+    };
+    const std::vector<TestData> testData = {
+        {"", '.', {""}},
+        {"11", '.', {"11"}},
+        {"..", '.', {"", "", ""}},
+        {"11.", '.', {"11", ""}},
+        {".11", '.', {"", "11"}},
+        {"11.22", '.', {"11", "22"}},
+    };
+
+    for (const auto & td : testData) {
+        ASSERT_EQ(otus::Split(td.instr, td.inchar), td.out);
+    }
+}
